78f2fcf77c53770e3a5bc8241fbb68b876c48d9c
chayma <chaymajlali2001l@gmail.com>
2025-05-06 19:37:51 +0100

    first commit

---
+++ app/src/main/java/com/example/healthcareapp/FindDoctorActivity.java
@@ -0,0 +1,28 @@
+package com.example.healthcareapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FindDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_doctor);
+        CardView exit = findViewById(R.id.cardFDBack);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FindDoctorActivity.this, HomeActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/healthcareapp/RegisterActivity2.java
@@ -0,0 +1,99 @@
+package com.example.healthcareapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterActivity2 extends AppCompatActivity {
+    EditText edUsername, edEamil, edPassword, edConfirm;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register2);
+        edUsername = findViewById(R.id.editTextRegUserName);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edEamil = findViewById(R.id.editTextRegEmail);
+        edConfirm = findViewById(R.id.editTextRegConfirmPassword);
+        btn = findViewById(R.id.buttonRegister);
+        tv = findViewById(R.id.textExistingUser);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity2.this, LoginActivity.class));
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String email = edEamil.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirm = edConfirm.getText().toString();
+
+                Database db=new Database (getApplicationContext(),"healthcare",null,1);
+
+                if (username.length() == 0 || email.length() == 0 || password.length() == 0 || confirm.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "please fill all details", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (password.compareTo(confirm) == 0) {
+                        if (isValid(password)) {
+                          db.register(username,email,password);//with this we succeed to enregister the database
+
+
+                            Toast.makeText(getApplicationContext(), "Record Inserted", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterActivity2.this,LoginActivity.class));
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "password must contain at least 8 characters,having letter and digit", Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                        Toast.makeText(getApplicationContext(), "password and confirm password didn't match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+        });
+    }
+
+    public static boolean isValid(String passwordhere) {
+        int f1 = 0, f2 = 0, f3 = 0;
+        if (passwordhere.length() < 8) {
+            return false;
+        } else {
+            for (int p = 0; p < passwordhere.length(); p++) {
+                if (Character.isLetter(passwordhere.charAt(p))) {
+                    f1 = 1;
+                }
+            }
+            for (int r = 0; r < passwordhere.length(); r++) {
+                if (Character.isDigit(passwordhere.charAt(r))) {
+                    f2 = 1;
+                }
+            }
+            for (int s = 0; s < passwordhere.length(); s++) {
+                char c = passwordhere.charAt(s);
+                if ((c >= 33 && c <= 46) || c == 64) {
+                    f3 = 1;
+                }
+            }
+            if (f1 == 1 && f2 == 1 && f3 == 1)
+                return true;
+            return false;
+        }
+    }
+}
